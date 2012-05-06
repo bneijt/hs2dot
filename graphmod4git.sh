77b3cf6dce126451ecd8dbf0c85ea0d154863aaf
A. Bram Neijt <bneijt@gmail.com>
2012-05-06 13:17:17 +0200

    Add graphmod4git to do graphing

---
+++ graphmod4git.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+REPO="$PWD"
+if [[ ! ${REPO} = /tmp* ]]; then
+    echo "This script will reset your git repo, so"
+    echo "You need to clone your repo in /tmp"
+    echo "and run it in that clone"
+    echo "e.a. git clone <path to local file>"
+    exit 1
+fi
+
+if [ ! -d dots ]; then
+    mkdir dots
+    commits="`git log --pretty=format:%H`"
+    for hash in $commits; do
+        git reset --hard "$hash"
+        graphmod -q --no-cluster Main.hs > dots/"$hash".dot
+        echo "${hash}.dot" >> dots/index.list
+    done
+fi
+echo "Calling dotimate"
+cd dots
+
+if [ ! -d frames ]; then
+    dotimate `cat index.list`
+fi
+
+echo "Fixing image sizes"
+for image in frames/fram*.jpg;do
+    convert "$image" -crop 1152x1152-0-0\! -background white -flatten "$image"
+done
+echo "Creating movie"
+ffmpeg -r 15 -vf "pad=1160:1160" -i "frames/frame%05d.jpg" movie.webm
+
